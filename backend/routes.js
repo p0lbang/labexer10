2952766dfd85f2fbb010928e3a2244ccaa6ce5d3
Van Paul Dayag <dayag.vanpaul@gmail.com>
2022-06-12 14:08:12 +0800

    make work in chrome

--- backend/routes.js
+++ backend/routes.js
@@ -18,7 +18,7 @@ import {
 const router = (app) => {
   // Allow Cross Origin Resource Sharing
   app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader(
       "Access-Control-Allow-Methods",
@@ -28,6 +28,7 @@ const router = (app) => {
       "Access-Control-Allow-Headers",
       "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
     );
+    
     next();
   });
 
